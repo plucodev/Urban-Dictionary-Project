7ef1396eccb699a4ab5f2afb3bdd151d1ec5c7f7
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-05-19 01:33:44 +0000

    updated

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import requests, os
 
 # Retrieve your API credentials from the .env file
 if os.getenv("API_KEY") is None or os.getenv("API_KEY") == "":
-    print("ERROR! Make sure you have created your .env file with your API credentials (look for the .evn.example as an example and replace it with your own API credentials that you got from RapidAPI)")
+    print("ERROR Make sure you have created your .env file with your API credentials (look for the .evn.example as an example and replace it with your own API credentials that you got from RapidAPI)")
     exit(1)
 
 # Get credentials from the .env file
